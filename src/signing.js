fef573b2fb6d534917651c4228d0abaa1639c068
Tommaso Barbugli <tbarbugli@gmail.com>
2019-11-28 16:49:06 +0100

    typo

--- src/signing.js
+++ src/signing.js
@@ -25,7 +25,7 @@ export function JWTUserToken(apiSecret, userId, extraData = {}, jwtOptions = {})
 	// make sure we return a clear error when jwt is shimmed (ie. browser build)
 	if (jwt == null || jwt.sign == null) {
 		throw Error(
-			`Unable to find jwt crypto, if you are getting this error is probably because you are trying to generate tokens on browser or React Native (or other environment where crypto functions are not available). Please Note: token should only be generate server-side.`,
+			`Unable to find jwt crypto, if you are getting this error is probably because you are trying to generate tokens on browser or React Native (or other environment where crypto functions are not available). Please Note: token should only be generated server-side.`,
 		);
 	}
 
