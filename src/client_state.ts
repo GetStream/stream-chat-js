ccd2134199271dd748755502bb871c9102e5178d
nhannah <ne.hannah@gmail.com>
2020-07-10 13:22:15 -0600

    client state ts

--- src/client_state.ts
+++ src/client_state.ts
@@ -1,30 +1,33 @@
 import Immutable from 'seamless-immutable';
+import type { User } from '../types/types';
 
 /**
  * ClientState - A container class for the client state.
  */
-export class ClientState {
+export class ClientState<UserType> {
+	users: Immutable.ImmutableObject<{ [key: string]: Immutable.Immutable<User<UserType>> }>
+	userChannelReferences: { [key: string]: { [key: string]: boolean }}
 	constructor() {
 		// show the status for a certain user...
 		// ie online, offline etc
-		this.users = Immutable({});
+		this.users = Immutable<{ [key: string]: Immutable.Immutable<User<UserType>> }>({});
 		// store which channels contain references to the specified user...
 		this.userChannelReferences = {};
 	}
 
-	updateUsers(users) {
+	updateUsers(users: User<UserType>[]) {
 		for (const user of users) {
 			this.updateUser(user);
 		}
 	}
 
-	updateUser(user) {
+	updateUser(user?: User<UserType>) {
 		if (user != null) {
 			this.users = this.users.set(user.id, Immutable(user));
 		}
 	}
 
-	updateUserReference(user, channelID) {
+	updateUserReference(user: User<UserType>, channelID: string) {
 		if (user == null) {
 			return;
 		}
