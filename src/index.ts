360a1317ff1d0f215278e0cb283e580e7840c999
Amin Mahboubi <aminmahboobi@gmail.com>
2020-08-13 12:38:05 +0200

    export star the client file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 export * from './base64';
-export { StreamChat } from './client';
+export * from './client';
 export * from './client_state';
 export * from './channel';
 export * from './channel_state';
 export * from './connection';
-export { isValidEventType, EVENT_MAP } from './events';
+export * from './events';
 export * from './permissions';
 export * from './signing';
 export * from './token_manager';
 export * from './types';
-export { logChatPromiseExecution } from './utils';
+export { chatCodes, logChatPromiseExecution } from './utils';
