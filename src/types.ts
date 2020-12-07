9b26c0db2a14bbff5852f58b54fa15f116fecb97
Anatoly Rugalev <anatoly.rugalev@gmail.com>
2020-12-07 10:24:16 +0300

    Rolled back webhook_id and webhook_failed message fields

--- src/types.ts
+++ src/types.ts
@@ -428,8 +428,6 @@ export type MessageResponse<
   status?: string;
   type?: string;
   updated_at?: string;
-  webhook_failed?: boolean;
-  webhook_id?: string;
 };
 
 export type MuteResponse<UserType = UnknownType> = {
