9a678dc7b6aceb0a773d1929cb8cb9a7f0e135ea
ferhat elmas <elmas.ferhat@gmail.com>
2021-02-15 16:22:24 +0100

    Add push notification flag into channel config

--- src/types.ts
+++ src/types.ts
@@ -736,6 +736,7 @@ export type CreateChannelOptions<CommandType extends string = LiteralStringForUn
   mutes?: boolean;
   name?: string;
   permissions?: PermissionObject[];
+  push_notifications?: boolean;
   reactions?: boolean;
   read_events?: boolean;
   replies?: boolean;
