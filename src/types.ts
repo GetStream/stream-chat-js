5f713f75096fb8889fa3496660092a01022c58b6
Marcelo Pires <thesyncim@gmail.com>
2021-05-18 11:32:19 +0100

    [CHAT-1513] partial message update (#576)
    * implement partial message update
    
    * fix docs
    
    * wip on tests
    
    * add todo list
    
    * improve test coverage
    
    * fix comments
    
    * rename types
    
    * exclude reserved fields
    
    * fix updatable fields
    
    Co-authored-by: Vishal Narkhede <vishalnarkhede.iitd@gmail.com>

--- src/types.ts
+++ src/types.ts
@@ -1620,7 +1620,8 @@ export type EndpointName =
   | 'ExportChannels'
   | 'GetExportChannelsStatus'
   | 'CheckSQS'
-  | 'GetRateLimits';
+  | 'GetRateLimits'
+  | 'MessageUpdatePartial';
 
 export type ExportChannelRequest = {
   id: string;
@@ -1707,6 +1708,13 @@ export type PartialUserUpdate<UserType = UnknownType> = {
   id: string;
   set?: Partial<UserResponse<UserType>>;
   unset?: Array<keyof UserResponse<UserType>>;
+};
+
+export type MessageUpdatableFields<MessageType = UnknownType> = Omit<MessageResponse<MessageType>, 'cid' | 'created_at' | 'updated_at' | 'deleted_at' | 'user' | 'user_id'>
+
+export type PartialMessageUpdate<MessageType = UnknownType> = {
+    set?: Partial<MessageUpdatableFields<MessageType>>;
+    unset?: Array<keyof MessageUpdatableFields<MessageType>>;
 };
 
 export type PermissionAPIObject = {
