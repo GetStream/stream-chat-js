300c2f0d6f00d2d375b5ee5fa50f8a1c55f3c0b9
Amin Mahboubi <amin@getstream.io>
2021-12-02 12:39:10 +0100

    Fix: Remove unused fileds in WS JOSN payload (#829)
    * Fix: Remove unused fileds in WS JOSN payload
    
    * fix tests

--- src/client.ts
+++ src/client.ts
@@ -2609,8 +2609,6 @@ export class StreamChat<
     return JSON.stringify({
       user_id: this.userID,
       user_details: this._user,
-      user_token: this.tokenManager.getToken(),
-      server_determines_connection_id: true,
       device: this.options.device,
       client_request_id,
     });
