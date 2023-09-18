8a2ad699e058016b61ecef91c8c782c4cb0c9be7
MartinCupela <32706194+MartinCupela@users.noreply.github.com>
2023-09-18 16:07:26 +0200

    feat: declare and export types SendMessageOptions and UpdateMessageOptions (#1170)

--- src/types.ts
+++ src/types.ts
@@ -2077,6 +2077,20 @@ export type MessageBase<
 
 export type MessageLabel = 'deleted' | 'ephemeral' | 'error' | 'regular' | 'reply' | 'system';
 
+export type SendMessageOptions = {
+  force_moderation?: boolean;
+  is_pending_message?: boolean;
+  keep_channel_hidden?: boolean;
+  pending?: boolean;
+  pending_message_metadata?: Record<string, string>;
+  skip_enrich_url?: boolean;
+  skip_push?: boolean;
+};
+
+export type UpdateMessageOptions = {
+  skip_enrich_url?: boolean;
+};
+
 export type Mute<StreamChatGenerics extends ExtendableGenerics = DefaultGenerics> = {
   created_at: string;
   target: UserResponse<StreamChatGenerics>;
