2df6a0edd01023de2b606fe70139f567a8c8b91c
Antti Kupila <akupila@gmail.com>
2024-08-06 14:34:59 +0300

    feat: add support for configuring partitioning

--- src/types.ts
+++ src/types.ts
@@ -947,6 +947,8 @@ export type CreateChannelOptions<StreamChatGenerics extends ExtendableGenerics =
   message_retention?: string;
   mutes?: boolean;
   name?: string;
+  partition_size?: number;
+  partition_ttl?: string | null;
   permissions?: PermissionObject[];
   polls?: boolean;
   push_notifications?: boolean;
