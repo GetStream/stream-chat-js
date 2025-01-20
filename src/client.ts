7c1c58df4d0c7cffdadf7062189053d9b7a5fb80
Varsh <mailvarbhat@gmail.com>
2025-01-20 12:23:21 +0100

    feat: add translate API support (#1415)
    Co-authored-by: Vishal Narkhede <vishalnarkhede.iitd@gmail.com>

--- src/client.ts
+++ src/client.ts
@@ -189,6 +189,7 @@ import {
   TestSNSDataInput,
   TestSQSDataInput,
   TokenOrProvider,
+  TranslateResponse,
   UnBanUserOptions,
   UpdateChannelOptions,
   UpdateChannelResponse,
@@ -2514,6 +2515,23 @@ export class StreamChat<StreamChatGenerics extends ExtendableGenerics = DefaultG
       this.baseURL + `/messages/${encodeURIComponent(messageId)}/translate`,
       { language },
     );
+  }
+
+  /**
+   * translate - translates the given text to provided language
+   *
+   * @param {string} text
+   * @param {string} destination_language
+   * @param {string} source_language
+   *
+   * @return {TranslateResponse} Response that includes the message
+   */
+  async translate(text: string, destination_language: string, source_language: string) {
+    return await this.post<APIResponse & TranslateResponse>(this.baseURL + `/translate`, {
+      text,
+      source_language,
+      destination_language,
+    });
   }
 
   /**
