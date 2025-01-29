871a58c1f21142aa389cbb7bc7c750d58e18910b
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-01-29 17:49:04 +0100

    chore: port channel pinning and archiving utils

--- src/utils.ts
+++ src/utils.ts
@@ -13,7 +13,9 @@ import {
   MessageSet,
   MessagePaginationOptions,
   ChannelQueryOptions,
-  QueryChannelAPIResponse,
+  QueryChannelAPIResponse, ChannelSort,
+  ChannelFilters,
+  ChannelSortBase
 } from './types';
 import { StreamChat } from './client';
 import { Channel } from './channel';
@@ -838,4 +840,130 @@ export const generateChannelTempCid = (channelType: string, members: string[]) =
   if (!membersStr) return;
   return `${channelType}:!members-${membersStr}`;
 };
+
+export const isChannelPinned = <
+  StreamChatGenerics extends ExtendableGenerics = DefaultGenerics,
+>(
+  channel: Channel<StreamChatGenerics>,
+) => {
+  if (!channel) return false;
+
+  const member = channel.state.membership;
+
+  return !!member?.pinned_at;
+};
+
+export const isChannelArchived = <
+  StreamChatGenerics extends ExtendableGenerics = DefaultGenerics,
+>(
+  channel: Channel<StreamChatGenerics>,
+) => {
+  if (!channel) return false;
+
+  const member = channel.state.membership;
+
+  return !!member?.archived_at;
+};
+
+export const shouldConsiderArchivedChannels = <
+  StreamChatGenerics extends ExtendableGenerics = DefaultGenerics,
+>(
+  filters: ChannelFilters<StreamChatGenerics>,
+) => {
+  if (!filters) return false;
+
+  return typeof filters.archived === 'boolean';
+};
+
+export const extractSortValue = <
+  StreamChatGenerics extends ExtendableGenerics = DefaultGenerics,
+>({
+    atIndex,
+    sort,
+    targetKey,
+  }: {
+  atIndex: number;
+  targetKey: keyof ChannelSortBase<StreamChatGenerics>;
+  sort?: ChannelSort<StreamChatGenerics>;
+}) => {
+  if (!sort) return null;
+  let option: null | ChannelSortBase<StreamChatGenerics> = null;
+
+  if (Array.isArray(sort)) {
+    option = sort[atIndex] ?? null;
+  } else {
+    let index = 0;
+    for (const key in sort) {
+      if (index !== atIndex) {
+        index++;
+        continue;
+      }
+
+      if (key !== targetKey) {
+        return null;
+      }
+
+      option = sort;
+
+      break;
+    }
+  }
+
+  return option?.[targetKey] ?? null;
+};
+
+/**
+ * Returns true only if `{ pinned_at: -1 }` or `{ pinned_at: 1 }` option is first within the `sort` array.
+ */
+export const shouldConsiderPinnedChannels = <
+  StreamChatGenerics extends ExtendableGenerics = DefaultGenerics,
+>(
+  sort: ChannelSort<StreamChatGenerics>,
+) => {
+  const value = extractSortValue({
+    atIndex: 0,
+    sort,
+    targetKey: 'pinned_at',
+  });
+
+  if (typeof value !== 'number') return false;
+
+  return Math.abs(value) === 1;
+};
+
+export function findPinnedAtSortOrder<
+  StreamChatGenerics extends ExtendableGenerics = DefaultGenerics,
+>({ sort }: { sort: ChannelSort<StreamChatGenerics> }) {
+  if (!sort) return null;
+
+  if (Array.isArray(sort)) {
+    const [option] = sort;
+
+    if (!option?.pinned_at) return null;
+
+    return option.pinned_at;
+  } else {
+    if (!sort.pinned_at) return null;
+
+    return sort.pinned_at;
+  }
+}
+
+export function findLastPinnedChannelIndex<
+  StreamChatGenerics extends ExtendableGenerics = DefaultGenerics,
+>({ channels }: { channels: Channel<StreamChatGenerics>[] }) {
+  let lastPinnedChannelIndex: number | null = null;
+
+  for (const channel of channels) {
+    if (!isChannelPinned(channel)) break;
+
+    if (typeof lastPinnedChannelIndex === 'number') {
+      lastPinnedChannelIndex++;
+    } else {
+      lastPinnedChannelIndex = 0;
+    }
+  }
+
+  return lastPinnedChannelIndex;
+}
 
