d86aba6f225ebde87deede5aac4d366c3620db4f
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-02-05 17:52:28 +0100

    chore: add tests for message.new

--- test/unit/channel_manager.test.ts
+++ test/unit/channel_manager.test.ts
@@ -7,7 +7,8 @@ import { generateChannel } from './test-utils/generateChannel';
 import { generateMember } from './test-utils/generateMember';
 import { generateUser } from './test-utils/generateUser';
 import { getClientWithUser } from './test-utils/getClient';
-import { afterEach } from 'mocha';
+import * as Utils from '../../src/utils';
+import { Channel } from 'node:diagnostics_channel';
 
 describe('ChannelManager', () => {
   let client: StreamChat;
@@ -25,14 +26,26 @@ describe('ChannelManager', () => {
   })
 
   describe('websocket event handlers', () => {
-    let setChannelsSpy: sinon.SinonSpy;
+    let setChannelsStub: sinon.SinonStub;
+    let isChannelPinnedStub: sinon.SinonStub;
+    let isChannelArchivedStub: sinon.SinonStub;
+    let shouldConsiderArchivedChannelsStub: sinon.SinonStub;
+    let shouldConsiderPinnedChannelsStub: sinon.SinonStub;
+    let moveChannelUpwardsSpy: sinon.SinonSpy;
     let channelToRemove: ChannelResponse;
 
     beforeEach(() => {
-      const channels = [generateChannel({ channel: { id: 'channel1' }}).channel, generateChannel({ channel: { id: 'channel2' }}).channel, generateChannel({ channel: { id: 'channel3' }}).channel]
+      const channelsResponse = [generateChannel({ channel: { id: 'channel1' }}), generateChannel({ channel: { id: 'channel2' }}), generateChannel({ channel: { id: 'channel3' }})]
+      client.hydrateActiveChannels(channelsResponse);
+      const channels = channelsResponse.map((c) => client.channel(c.channel.type, c.channel.id));
       channelManager.state.partialNext({ channels, ready: true })
-      setChannelsSpy = sinon.spy(channelManager, 'setChannels');
-      channelToRemove = channels[1];
+      setChannelsStub = sinon.stub(channelManager, 'setChannels');
+      isChannelPinnedStub = sinon.stub(Utils, 'isChannelPinned');
+      isChannelArchivedStub = sinon.stub(Utils, 'isChannelArchived');
+      shouldConsiderArchivedChannelsStub = sinon.stub(Utils, 'shouldConsiderArchivedChannels');
+      shouldConsiderPinnedChannelsStub = sinon.stub(Utils, 'shouldConsiderPinnedChannels');
+      moveChannelUpwardsSpy = sinon.spy(Utils, 'moveChannelUpwards');
+      channelToRemove = channelsResponse[1];
     })
 
     afterEach(() => {
@@ -48,21 +61,21 @@ describe('ChannelManager', () => {
           client.dispatchEvent({ type: eventType, cid: channelToRemove.cid })
           client.dispatchEvent({ type: eventType, channel: channelToRemove })
 
-          expect(setChannelsSpy.calledOnce).to.be.false;
+          expect(setChannelsStub.called).to.be.false;
         });
 
         it('should remove the channel when event.cid matches', () => {
           client.dispatchEvent({ type: eventType, cid: channelToRemove.cid })
 
-          expect(setChannelsSpy.calledOnce).to.be.true;
-          expect(setChannelsSpy.args[0][0].map((c: ChannelResponse) => c.id)).to.deep.equal(['channel1', 'channel3']);
+          expect(setChannelsStub.calledOnce).to.be.true;
+          expect(setChannelsStub.args[0][0].map((c: ChannelResponse) => c.id)).to.deep.equal(['channel1', 'channel3']);
         });
 
         it('should remove the channel when event.channel?.cid matches', () => {
           client.dispatchEvent({ type: eventType, channel: channelToRemove })
 
-          expect(setChannelsSpy.calledOnce).to.be.true;
-          expect(setChannelsSpy.args[0][0].map((c: ChannelResponse) => c.id)).to.deep.equal(['channel1', 'channel3']);
+          expect(setChannelsStub.calledOnce).to.be.true;
+          expect(setChannelsStub.args[0][0].map((c: ChannelResponse) => c.id)).to.deep.equal(['channel1', 'channel3']);
         });
 
         it('should not modify the list if no channels match', () => {
@@ -70,11 +83,134 @@ describe('ChannelManager', () => {
           client.dispatchEvent({ type: eventType, cid: 'channel123' })
           const { channels: newChannels } = channelManager.state.getLatestValue();
 
-          expect(setChannelsSpy.calledOnce).to.be.false;
+          expect(setChannelsStub.called).to.be.false;
           expect(prevChannels).to.equal(newChannels)
           expect(prevChannels).to.deep.equal(newChannels);
         });
       })
     });
+
+    describe('newMessageHandler', () => {
+      it('should not update the state early if channels are not defined', () => {
+        channelManager.state.partialNext({ channels: undefined })
+
+        client.dispatchEvent({ type: 'message.new', channel_type: 'messaging', channel_id: 'channel2' })
+
+        expect(setChannelsStub.called).to.be.false;
+      });
+
+      it('should not update the state if channel is pinned and sorting considers pinned channels', () => {
+        const { channels: prevChannels } = channelManager.state.getLatestValue();
+        isChannelPinnedStub.returns(true);
+        shouldConsiderPinnedChannelsStub.returns(true);
+
+        client.dispatchEvent({ type: 'message.new', channel_type: 'messaging', channel_id: 'channel2' })
+
+        const { channels: newChannels } = channelManager.state.getLatestValue();
+
+        expect(setChannelsStub.called).to.be.false;
+        expect(prevChannels).to.equal(newChannels);
+        expect(prevChannels).to.deep.equal(newChannels);
+      });
+
+      it('should not update the state if channel is archived and sorting considers archived channels, but the filter is false', () => {
+        const { channels: prevChannels } = channelManager.state.getLatestValue();
+        channelManager.state.next(prevState => ({ ...prevState, pagination: { ...prevState.pagination, filters: { archived: false }}}))
+        isChannelArchivedStub.returns(true);
+        shouldConsiderArchivedChannelsStub.returns(true);
+
+        client.dispatchEvent({ type: 'message.new', channel_type: 'messaging', channel_id: 'channel2' })
+
+        const { channels: newChannels } = channelManager.state.getLatestValue();
+
+        expect(setChannelsStub.called).to.be.false;
+        expect(prevChannels).to.equal(newChannels);
+        expect(prevChannels).to.deep.equal(newChannels);
+      });
+
+      it('should not update the state if channel is not archived and sorting considers archived channels, but the filter is true', () => {
+        const { channels: prevChannels } = channelManager.state.getLatestValue();
+        channelManager.state.next(prevState => ({ ...prevState, pagination: { ...prevState.pagination, filters: { archived: true }}}))
+        isChannelArchivedStub.returns(false);
+        shouldConsiderArchivedChannelsStub.returns(true);
+
+        client.dispatchEvent({ type: 'message.new', channel_type: 'messaging', channel_id: 'channel2' })
+
+        const { channels: newChannels } = channelManager.state.getLatestValue();
+
+        expect(setChannelsStub.called).to.be.false;
+        expect(prevChannels).to.equal(newChannels);
+        expect(prevChannels).to.deep.equal(newChannels);
+      });
+
+      it('should not update the state if channelManager.options.lockChannelOrder is true', () => {
+        const { channels: prevChannels } = channelManager.state.getLatestValue();
+        channelManager.setOptions({ lockChannelOrder: true });
+
+        client.dispatchEvent({ type: 'message.new', channel_type: 'messaging', channel_id: 'channel2' })
+
+        const { channels: newChannels } = channelManager.state.getLatestValue();
+
+        expect(setChannelsStub.called).to.be.false;
+        expect(prevChannels).to.equal(newChannels);
+        expect(prevChannels).to.deep.equal(newChannels);
+
+        channelManager.setOptions({});
+      });
+
+      it('should not update the state if the channel is not part of the list and allowNewMessagesFromUnfilteredChannels if false', () => {
+        const { channels: prevChannels } = channelManager.state.getLatestValue();
+        isChannelPinnedStub.returns(false);
+        isChannelArchivedStub.returns(false);
+        shouldConsiderArchivedChannelsStub.returns(false);
+        shouldConsiderPinnedChannelsStub.returns(false);
+        channelManager.setOptions({ allowNewMessagesFromUnfilteredChannels: false });
+
+        client.dispatchEvent({ type: 'message.new', channel_type: 'messaging', channel_id: 'channel4' })
+
+        const { channels: newChannels } = channelManager.state.getLatestValue();
+
+        expect(setChannelsStub.called).to.be.false;
+        expect(prevChannels).to.equal(newChannels);
+        expect(prevChannels).to.deep.equal(newChannels);
+
+        channelManager.setOptions({});
+      });
+
+      it('should move the channel upwards if it is not part of the list and allowNewMessagesFromUnfilteredChannels is true', () => {
+        isChannelPinnedStub.returns(false);
+        isChannelArchivedStub.returns(false);
+        shouldConsiderArchivedChannelsStub.returns(false);
+        shouldConsiderPinnedChannelsStub.returns(false);
+
+        client.dispatchEvent({ type: 'message.new', channel_type: 'messaging', channel_id: 'channel4' })
+
+        const { pagination: { sort }, channels } = channelManager.state.getLatestValue();
+        const moveChannelUpwardsArgs = moveChannelUpwardsSpy.args[0][0];
+        const newChannel = client.channel('messaging', 'channel4')
+
+        expect(setChannelsStub.calledOnce).to.be.true;
+        expect(moveChannelUpwardsSpy.calledOnce).to.be.true;
+        expect(moveChannelUpwardsArgs).to.deep.equal({ channels, channelToMove: newChannel, channelToMoveIndexWithinChannels: -1, sort });
+        expect(setChannelsStub.args[0][0]).to.deep.equal(Utils.moveChannelUpwards(moveChannelUpwardsArgs))
+      });
+
+      it('should move the channel upwards if all conditions allow it', () => {
+        isChannelPinnedStub.returns(false);
+        isChannelArchivedStub.returns(false);
+        shouldConsiderArchivedChannelsStub.returns(false);
+        shouldConsiderPinnedChannelsStub.returns(false);
+
+        client.dispatchEvent({ type: 'message.new', channel_type: 'messaging', channel_id: 'channel2' })
+
+        const { pagination: { sort }, channels } = channelManager.state.getLatestValue();
+        const moveChannelUpwardsArgs = moveChannelUpwardsSpy.args[0][0];
+
+        expect(setChannelsStub.calledOnce).to.be.true;
+        expect(moveChannelUpwardsSpy.calledOnce).to.be.true;
+        expect(moveChannelUpwardsArgs).to.deep.equal({ channels, channelToMove: channels[1], channelToMoveIndexWithinChannels: 1, sort });
+        expect(setChannelsStub.args[0][0]).to.deep.equal(Utils.moveChannelUpwards(moveChannelUpwardsArgs))
+      });
+    })
   })
 })
