98672f8e27678ed85dcfe1410c9831188e7723d7
Anton Arnautov <arnautov.anton@gmail.com>
2025-02-14 17:58:50 +0100

    Initial draft

---
+++ scripts/bundle-cjs.mjs
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+import { resolve } from 'node:path';
+import * as esbuild from 'esbuild';
+import packageJson from '../package.json' with {'type': 'json'};
+
+// import.meta.dirname is not available before Node 20
+const __dirname = import.meta.dirname;
+
+const mainEntrypoint = resolve(__dirname, '../src/index.ts');
+const outDir = resolve(__dirname, '../dist');
+
+// Those dependencies are distributed as ES modules, and cannot be externalized
+// in our CJS bundle. We convert them to CJS and bundle them instead.
+const bundledDeps = [
+  'axios',
+  'form-data',
+  'isomorphic-ws',
+  'base64-js',
+  // 'jsonwebtoken', 'crypto', 'https'
+];
+
+const deps = Object.keys({
+  ...packageJson.dependencies,
+  ...packageJson.peerDependencies,
+});
+const external = deps.filter((dep) => !bundledDeps.includes(dep));
+
+/** @type esbuild.BuildOptions */
+const cjsBundleConfig = {
+  entryPoints: [mainEntrypoint],
+  bundle: true,
+  format: 'cjs',
+  target: 'es2015',
+  external,
+  outdir: outDir,
+  outExtension: { '.js': '.cjs' },
+  sourcemap: 'linked',
+};
+
+const browserPlugin = {
+  name: 'ignoreBrowser',
+  setup: (build) => {
+    const options = build.initialOptions;
+
+    if (options.platform !== 'browser') return;
+
+    build.onResolve({ filter: /(node:.+)|(jsonwebtoken)/ }, async ({ path }) => ({ path, external: true }));
+  },
+};
+
+// We build two CJS bundles: for browser and for node. The latter one can be
+// used e.g. during SSR (although it makes little sence to SSR chat, but still
+// nice for import not to break on server).
+const bundles = ['browser', 'node'].map((platform) => {
+  const config = {
+    ...cjsBundleConfig,
+    entryNames: `[dir]/[name].${platform}`,
+    platform,
+    define: {
+      'process.env.PKG_VERSION': JSON.stringify(packageJson.version),
+    },
+  };
+
+  if (platform === 'browser') {
+    config.plugins = [browserPlugin];
+  }
+
+  esbuild.build(config);
+});
+await Promise.all(bundles);
