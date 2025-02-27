0acd60bafe356e0333e30a4582c956d54f6453d1
Anton Arnautov <arnautov.anton@gmail.com>
2025-02-27 19:14:06 +0100

    chore: adjust release process, use package.json#version instead

--- scripts/get-package-version.mjs
+++ scripts/get-package-version.mjs
@@ -1,20 +1,13 @@
 import { execSync } from 'node:child_process';
-import { resolve } from 'node:path';
-import { readFileSync } from 'node:fs';
 import packageJson from '../package.json' with { type: 'json' };
 
-// Get the latest version so that magic string __STREAM_CHAT_REACT_VERSION__ can be replaced with it in the source code (used for reporting purposes)
+// get the latest version so that "process.env.PKG_VERSION" can be replaced with it in the source code (used for reporting purposes), see bundle.mjs for source
 export default function getPackageVersion() {
-  let version;
-  // During release, use the version being released
-  // see .releaserc.json where the .version file is generated
-  try {
-    console.info({ v: packageJson.version });
-    version = readFileSync(resolve(import.meta.dirname, '../.version')).toString().trim();
-  } catch {/* do nothing */}
+  // "build" script ("prepack" hook) gets invoked when semantic-release runs "npm publish", at that point package.json#version already contains updated next version which we can use
+  let version = packageJson.version;
 
-  if (typeof version !== 'string') {
-    // Otherwise use the latest git tag
+  // if it fails (loads a default), try pulling version from git
+  if (version === '0.0.0-development') {
     try {
       version = execSync('git describe --tags --abbrev=0').toString().trim();
     } catch (error) {
@@ -23,6 +16,8 @@ export default function getPackageVersion() {
       version = packageJson.version;
     }
   }
+
   console.log(`Determined the build package version to be ${version}`);
+
   return version;
 }
