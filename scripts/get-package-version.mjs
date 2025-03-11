32c5944ba2721ecd27f13776ec603ff3559ffe0a
Anton Arnautov <arnautov.anton@gmail.com>
2025-03-11 11:28:37 +0100

    chore: build, version & release adjustments

--- scripts/get-package-version.mjs
+++ scripts/get-package-version.mjs
@@ -3,17 +3,18 @@ import packageJson from '../package.json' with { type: 'json' };
 
 // get the latest version so that "process.env.PKG_VERSION" can be replaced with it in the source code (used for reporting purposes), see bundle.mjs for source
 export default function getPackageVersion() {
-  // "build" script ("prepack" hook) gets invoked when semantic-release runs "npm publish", at that point package.json#version already contains updated next version which we can use
+  // "build" script ("prepare" hook) gets invoked when semantic-release runs "npm publish", at that point package.json#version already contains updated next version which we can use
   let version = packageJson.version;
+
+  console.log({ npm_package_version: process.env.npm_package_version });
 
   // if it fails (loads a default), try pulling version from git
   if (version === '0.0.0-development') {
     try {
       version = execSync('git describe --tags --abbrev=0').toString().trim();
     } catch (error) {
-      console.error(error);
       console.warn(
-        'Could not get latest version from git tags, falling back to package.json',
+        'Could not get latest version from git tags, falling back to package.json#version',
       );
       version = packageJson.version;
     }
