efcd75a332f8e180b234acb4d9f30b1984aca6aa
Anton Arnautov <arnautov.anton@gmail.com>
2025-05-04 14:06:07 +0200

    No need to re-check text equality

--- src/messageComposer/messageComposer.ts
+++ src/messageComposer/messageComposer.ts
@@ -478,8 +478,7 @@ export class MessageComposer extends WithSubscriptions {
           return;
         }
 
-        const [previousText] = previousSelection;
-        if (!this.linkPreviewsManager.enabled || currentText === previousText) return;
+        if (!this.linkPreviewsManager.enabled) return;
 
         if (!currentText) {
           this.linkPreviewsManager.clearPreviews();
