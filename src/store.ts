5408ab357c5ef497f899d293dbc5c8a785bc6670
Anton Arnautov <arnautov.anton@gmail.com>
2025-05-11 16:56:13 +0200

    Add store tests

--- src/store.ts
+++ src/store.ts
@@ -154,8 +154,11 @@ export class StateStore<T extends Record<string, unknown>> {
  *
  * @template O The type of the original state store
  * @template M The type of the merged state store
+ *
+ * @experimental
+ * This class is experimental and may change in future versions.
  */
-class MergedStateStore<
+export class MergedStateStore<
   O extends Record<string, unknown>,
   M extends Record<string, unknown>,
 > extends StateStore<O & M> {
@@ -292,8 +295,7 @@ class MergedStateStore<
   }
 }
 
-// EXAMPLE:
-
+/** EXAMPLE:
 const Uninitialized = Symbol('uninitialized');
 
 const b = new StateStore<{
@@ -327,3 +329,4 @@ a.subscribe((ns) => console.log(ns));
 a.original.partialNext({ next: 'next' });
 a.original.partialNext({ next: null });
 a.original.partialNext({ next: Uninitialized });
+*/
