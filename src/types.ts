d6bec1e2e3713f073d5707397979b44602c7aea8
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-05-27 12:36:34 +0200

    fix: remove faulty type

--- src/types.ts
+++ src/types.ts
@@ -1331,10 +1331,6 @@ export type StreamChatOptions = AxiosRequestConfig & {
    * that also relies on WS events will break these functionalities, so please use carefully.
    */
   disableCache?: boolean;
-  /**
-   * When enabled, message and thread reply drafts will be stored on the Stream server. This allows drafts to persist across devices and sessions.
-   */
-  drafts?: boolean;
   enableInsights?: boolean;
   /** experimental feature, please contact support if you want this feature enabled for you */
   enableWSFallback?: boolean;
