71e471592a351085c069e81c01d7cc6711d5b37f
Ivan Sekovanikj <31964049+isekovanic@users.noreply.github.com>
2025-07-23 12:09:57 +0200

    feat: query channels request override (#1583)
    ## CLA
    
    - [ ] I have signed the [Stream
    CLA](https://docs.google.com/forms/d/e/1FAIpQLScFKsKkAJI7mhCr7K9rEIOpqIDThrWxuvxnwUq2XkHyG154vQ/viewform)
    (required).
    - [ ] Code changes are tested
    
    ## Description of the changes, What, Why and How?
    
    This PR introduces a way for integrators to override the default
    `ChannelManager`'s `queryChannels` (which is essentially
    `client.queryChannels`.
    
    This is particularly useful in certain niche' edge cases, where for
    example if querying channels relies heavily on `{ cid: { $in: [...] }}`
    filters (or something similar) it would be impossible to properly
    paginate through everything as changing the `filters` on the fly causes
    the entire state to be wiped as it's considered to be a brand new query.
    
    It also comes with a set of rules:
    
    - `client.queryChannels` must always be invoked by this override (the
    reason being that `queryChannels` additionally does a bunch of state
    updates under the hood, so it's mandatory that this is run)
    - The `filters` stay intact during all of this (if they change, the
    `ChannelManager` will consider this to be a brand new query and wipe
    everything)
    - The override still needs to respect `limit` and `offset`, so that
    pagination works properly (this comes as a given since we pin specific
    IDs, but worth mentioning still)
    
    ## Changelog
    
    -

--- src/client.ts
+++ src/client.ts
@@ -233,6 +233,7 @@ import { PollManager } from './poll_manager';
 import type {
   ChannelManagerEventHandlerOverrides,
   ChannelManagerOptions,
+  QueryChannelsRequestType,
 } from './channel_manager';
 import { ChannelManager } from './channel_manager';
 import { NotificationManager } from './notifications';
@@ -720,10 +721,18 @@ export class StreamChat {
   createChannelManager = ({
     eventHandlerOverrides = {},
     options = {},
+    queryChannelsOverride,
   }: {
     eventHandlerOverrides?: ChannelManagerEventHandlerOverrides;
     options?: ChannelManagerOptions;
-  }) => new ChannelManager({ client: this, eventHandlerOverrides, options });
+    queryChannelsOverride?: QueryChannelsRequestType;
+  }) =>
+    new ChannelManager({
+      client: this,
+      eventHandlerOverrides,
+      options,
+      queryChannelsOverride,
+    });
 
   /**
    * Creates a new WebSocket connection with the current user. Returns empty promise, if there is an active connection
