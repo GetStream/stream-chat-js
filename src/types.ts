14746ba90bdbe08b413c8dfbad9cecaf1a2c05ab
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2025-08-19 10:02:19 +0200

    feat: add product to event hook request and response

--- src/types.ts
+++ src/types.ts
@@ -4206,7 +4206,7 @@ export type EventHook = {
   id?: string;
   hook_type?: HookType;
   enabled?: boolean;
-  product?: Product;
+  product?: Product | 'global'; // optional, default is 'global'
   event_types?: Array<string>;
   webhook_url?: string;
   sqs_queue_url?: string;
