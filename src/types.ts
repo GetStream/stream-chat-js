fb04b0e4770423bffa46c414983b02c5901bc2dd
Aditya Alif Nugraha <aditya.nugraha@getstream.io>
2025-08-25 11:16:19 +0200

    feat: add product to event hook request and response (#1603)
    ## CLA
    
    - [x] I have signed the [Stream
    CLA](https://docs.google.com/forms/d/e/1FAIpQLScFKsKkAJI7mhCr7K9rEIOpqIDThrWxuvxnwUq2XkHyG154vQ/viewform)
    (required).
    - [x] Code changes are tested
    
    ## Description of the changes, What, Why and How?
    
    We are introducing a product-scoped hook config that allows
    configuration for specific products. When a product-scoped config is
    created, changes will not affect other products.
    
    ## Changelog
    
    - Add `product` to `EventHook` type

--- src/types.ts
+++ src/types.ts
@@ -4206,6 +4206,7 @@ export type EventHook = {
   id?: string;
   hook_type?: HookType;
   enabled?: boolean;
+  product?: Product | 'all'; // optional, default is 'all'
   event_types?: Array<string>;
   webhook_url?: string;
   sqs_queue_url?: string;
