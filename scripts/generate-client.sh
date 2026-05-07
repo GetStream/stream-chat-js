b4f37ad8cf8ebcedb4c8eb32691175a160531f5f
Anton Arnautov <arnautov.anton@gmail.com>
2026-05-07 15:04:19 +0200

    Prepare generated files

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -6,6 +6,6 @@ CHAT_DIR="../chat"
 
 rm -rf $OUTPUT_DIR
 
-( cd $CHAT_DIR ; make openapi ; go run ./cmd/chat-manager openapi generate-client --language ts --spec ./releases/v2/chat-serverside-api.yaml --output $OUTPUT_DIR )
+( cd $CHAT_DIR ; make openapi ; go run ./cmd/chat-manager openapi generate-client --language ts --spec ./releases/v2/chat-clientside-api.yaml --output $OUTPUT_DIR )
 
 yarn lint-fix
