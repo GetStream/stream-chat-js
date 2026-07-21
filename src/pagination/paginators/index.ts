0eef85ecae764c45f1a0abfa8fed3793c036dcb7
martincupela <martin.cupela@gmail.com>
2026-07-21 14:59:51 +0200

    refactor: remove unused MessageReplyPaginator

--- src/pagination/paginators/index.ts
+++ src/pagination/paginators/index.ts
@@ -2,7 +2,6 @@ export * from './BasePaginator';
 export * from './ChannelPaginator';
 export { MessageIntervalPaginator } from './MessageIntervalPaginator';
 export * from './MessagePaginator';
-export * from './MessageReplyPaginator';
 export * from './PinnedMessagePaginator';
 export * from './ReminderPaginator';
 export * from './UserGroupPaginator';
